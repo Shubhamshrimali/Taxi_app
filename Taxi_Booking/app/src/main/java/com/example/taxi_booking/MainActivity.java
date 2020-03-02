9a05ac0631cdbba1d8052f27595045e423973e6e
shubham <shubham.shrimali@intel.com>
2020-03-02 15:27:49 +0530

    Commit

---
+++ Taxi_Booking/app/src/main/java/com/example/taxi_booking/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.taxi_booking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button mDriver, mCustomer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDriver = findViewById(R.id.driver);
+        mCustomer = findViewById(R.id.customer);
+        Intent i =new Intent(MainActivity.this,onAppKilled.class);
+        startService(i);
+
+        mDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        mCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
---
+++ Taxi_Booking/app/src/main/java/com/example/taxi_booking/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.example.taxi_booking;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID="ENTER_KEY";
+}
